da1077000eb6e5a307765d8aa191f3ab2ab28935
HyunIn Sim <>
2020-06-03 21:36:36 +0900

    Add Login Authentic 20200603

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken')
 
 const saltRounds = 10;
 
@@ -49,9 +50,44 @@ userSchema.pre("save", function (next) {
         next();
       });
     });
+  }else{
+    next() //비밀번호 수정아 아니면 다음으로....
   }
 });
 
+userSchema.methods.comparePassword= function(plainPassword, cb){
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+    if(err) return cb(err)
+   cb(null, isMatch)
+
+  })
+}
+
+
+userSchema.methods.generateToken = function(cb){
+  var user = this
+  var token = jwt.sign(user._id.toHexString(), 'secretToken')
+  user.token = token
+  user.save(function(err, user){
+    if(err) return cb(err)
+    cb(null, user)
+  })  
+}
+
+
+userSchema.statics.findByToken = function(token, cb) {
+  var user = this;
+  // user._id + ''  = token
+  //토큰을 decode 한다. 
+  jwt.verify(token, 'secretToken', function (err, decoded) {
+      //유저 아이디를 이용해서 유저를 찾은 다음에 
+      //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+      user.findOne({ "_id": decoded, "token": token }, function (err, user) {
+          if (err) return cb(err);
+          cb(null, user)
+      })
+  })
+}
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
